88f201bce680d361d81cbefcf807b8f5d1af5684
teban2006soto <105441640+teban2006soto@users.noreply.github.com>
2022-09-12 16:06:57 -0500

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,24 @@
+export const data =
+[
+    {
+        "id":1,
+        "name":"dt",
+        "image":"/img/dt 1.jpg"
+    },
+    {
+        "id":2,
+        "name":"rx",
+        "image":"/img/rx 1.jpg"
+    },
+    {
+        "id":3,
+        "name":"yz",
+        "image":"/img/yz 1.png"
+    },
+    {
+        "id":4,
+        "name":"xtz",
+        "image":"/img/xtz 1.jpg"
+    },
+
+]
